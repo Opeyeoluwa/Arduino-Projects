4fe9f551d05b5e17e807330c72f13c7d133294ac
Opeyeoluwa Olanipekun <96929543+Opeyeoluwa@users.noreply.github.com>
2022-05-18 17:25:59 -0400

    Update Relay_Over_Wifi_Blynk.ino

--- Relay_Over_Wifi_Blynk/Relay_Over_Wifi_Blynk.ino
+++ Relay_Over_Wifi_Blynk/Relay_Over_Wifi_Blynk.ino
@@ -1,27 +1,3 @@
-#define BLYNK_PRINT Serial        
-#include <ESP8266WiFi.h>        
-#include <BlynkSimpleEsp8266.h>        
-char auth[] = "authtocken"; // PASTE YOUR AUTHENTICATION CODE HERE        
-// Your WiFi credentials.        
-char ssid[] = "Rain Nigeria Airtel"; //WIFI NAME        
-char pass[] = "Rainigeria."; //PASSWORD   
-     
-void setup()        
-{        
- Serial.begin(9600);        
- Blynk.begin(auth, ssid, pass);        
-} 
-       
-void loop()        
-{        
- Blynk.run();        
-}       
-
-
-
-
-
-
 
 #define BLYNK_PRINT Serial  
 #include <ESP8266WiFi.h>  
